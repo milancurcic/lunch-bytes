913cdb4a6fb0af57bccf28f71034932f5bf729e8
lsiqueira <lsiqueira@rsmas.miami.edu>
2015-10-28 18:51:26 -0400

    Adding folders and files for python presentation on 10/28/15 at RSMAS

---
+++ LB03/banking.py
@@ -0,0 +1,23 @@
+class BankAccount:
+    def __init__(self):
+        self.balance = 0
+
+    def withdraw(self, amount):
+        self.balance -= amount
+        return self.balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self.balance
+
+class MinimumBalanceAccount(BankAccount):
+    def __init__(self, minimum_balance):
+        BankAccount.__init__(self)
+        self.minimum_balance = minimum_balance
+
+    def withdraw(self, amount):
+        if self.balance - amount < self.minimum_balance:
+            print 'Sorry, minimum balance must be maintained.'
+        else:
+            BankAccount.withdraw(self, amount)
+            return self.balance
